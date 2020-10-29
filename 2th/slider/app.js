456765644cfcdf57304e47b002f63df371086ea9
ihatelethargy <naturalflavor1007@naver.com>
2020-10-30 08:10:54 +0900

    silder-changing order

--- 2th/slider/app.js
+++ 2th/slider/app.js
@@ -1,34 +1,38 @@
+//  Start Img & Btn
 const carouselSlide = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
 
-// Btn
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
+// End Img & Btn
 
-// counter
+
+// Start Counter Default Setting
 let counter = 1;
 const size = carouselImages[0].clientWidth;
 
-
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+// End Counter Default Setting
 
 
-//Button Listenr
-
+// Start Button Listener(++)
 nextBtn.addEventListener('click',() => {
     if(counter >= carouselImages.length -1) return; // bug
     carouselSlide.style.transition = "transform 1s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
 })
+// End Button Listener(++)
 
 
+// Start Button Listener(--)
 prevBtn.addEventListener('click',() => {
     if(counter <= 0) return; // bug
     carouselSlide.style.transition = "transform 1s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
 })
+// End Button Listener(--)
 
 
 carouselSlide.addEventListener('transitionend', ()=>{
