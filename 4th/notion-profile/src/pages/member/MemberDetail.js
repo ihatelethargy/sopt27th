92fc0ef10d93e28dec893eb39e20f2a0691d560b
ihatelethargy <naturalflavor1007@naver.com>
2020-11-13 20:41:54 +0900

    memberDetail + memberList 수정

---
+++ 4th/notion-profile/src/pages/member/MemberDetail.js
@@ -0,0 +1,7 @@
+function MemberDetail() {
+    return (
+        <h1>MemberDetail</h1>
+    );
+}
+
+export default MemberDetail; 
---
+++ 4th/notion-profile/src/pages/member/MemverList.js
@@ -0,0 +1,7 @@
+function MemberList() {
+    return (
+        <h1>MemberList</h1>
+    );
+}
+
+export default MemberList; 
