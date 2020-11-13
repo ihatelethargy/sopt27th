3a7bd665bfe66afc9bad8242bf3c69d49b9f68d3
ihatelethargy <naturalflavor1007@naver.com>
2020-11-13 21:04:22 +0900

    no image

--- 4th/notion-profile/src/pages/member/Member.js
+++ 4th/notion-profile/src/pages/member/Member.js
@@ -3,11 +3,11 @@ import MemberList from './MemberList';
 import MemberDetail from './MemberDetail';
 
 function Member({ match }) {
-    console.log(match);
+    //console.log(match);
     return (
         <section style={{margin: "0 90px"}}>
             <Switch>
-                <Route exact path={match.path} component={MemberList}/>
+                <Route exact path={match.path} component={MemberList} />
                 <Route path={`${match.path}/:id`} component={MemberDetail}/>
             </Switch>
         </section>
