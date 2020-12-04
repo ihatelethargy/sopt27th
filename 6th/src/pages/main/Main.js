5f2523f5c961506cc7c9adcf0028eb1cf9c1f223
ihatelethargy <naturalflavor1007@naver.com>
2020-12-04 13:17:03 +0900

    이미지 경로

---
+++ 6th/src/pages/main/Main.js
@@ -0,0 +1,5 @@
+function Main() {
+    return (<h1> ON SOPT 27th <br /> Web Part 6th seminar</h1>);
+}
+
+export default Main;
---
+++ 6th/src/pages/member/Member.js
@@ -0,0 +1,18 @@
+import { Switch, Route } from 'react-router-dom';
+import MemberList from './MemberList';
+import MemberDetail from './MemberDetail';
+
+function Member({ match }) {
+    console.log(match.path);
+    return (
+        <section style={{margin: "0 90px"}}>
+            <Switch>
+                <Route exact path={match.path} component={MemberList} />
+                <Route path={`${match.path}/:id`} component={MemberDetail}/>
+            </Switch>
+        </section>
+    );
+}
+
+
+export default Member;
