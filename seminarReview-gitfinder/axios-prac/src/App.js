f3bc7231146c56051433d1226dbed56c51c958af
ihatelethargy <naturalflavor1007@naver.com>
2020-12-08 20:23:28 +0900

    api setting

--- seminarReview-gitfinder/axios-prac/src/App.js
+++ seminarReview-gitfinder/axios-prac/src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchInput from "./components/SearchInput";
 import SearchResult from "./components/SearchResult";
+import getUserAPI from "./lib/api";
 
 const App = (props) => {
-  return <></>;
+  const [userState, setUserState] = useState({
+    user: null,
+    status: "idle",
+  });
+
+  const getUser = (username) => {
+    console.log(username);
+    setUserState({ ...userState, status: "pendding" });
+
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <SearchInput onSubmit={getUser} />
+      <SearchResult />
+    </>
+  );
 };
 
 export default App;
--- seminarReview-gitfinder/axios-prac/src/components/SearchInput.js
+++ seminarReview-gitfinder/axios-prac/src/components/SearchInput.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchInput = (props) => {
-  return <></>;
+const SearchInput = ({ onSubmit }) => {
+  const [input, setInput] = useState(null);
+
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    onSubmit(input);
+  }
+
+  const changeHandler = (e) => {
+    setInput(e.target.value);
+    
+  };
+
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="git 계정을 입력하세요."
+          onChange={changeHandler}
+          value={input}
+        />
+      </form>
+    </>
+  );
 };
 
 export default SearchInput;
