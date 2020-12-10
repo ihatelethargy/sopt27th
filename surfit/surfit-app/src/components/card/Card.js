ba9bdb5b0422a7b18f2eb663e72a9403e6519c57
ihatelethargy <naturalflavor1007@naver.com>
2020-12-11 02:25:17 +0900

    card styling

--- surfit/surfit-app/src/components/card/Card.js
+++ surfit/surfit-app/src/components/card/Card.js
@@ -4,7 +4,24 @@ import "./Card.scss";
 const Card = ({ cardContent }) => {
   return (
     <>
-      <div className="cardWrapper">{cardContent.title}</div>
+      <div className="cardWrapper">
+        <img
+          className="card__content__img"
+          src={cardContent.contentUrl}
+          alt="card-img"
+        />
+        <div className="card__content__text">
+          <div className="card__content__title card_font">
+            {cardContent.title}
+          </div>
+          <div className="card__content__desc card_font">
+            {cardContent.description}
+          </div>
+          <div className="card__content__hashtag card_font">
+            {cardContent.hashtag}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
